73d3f5ac7d5f290e45b857273df1ba102b77e7f7
Filippo95 <filippo.luppi@gmail.com>
2018-01-26 14:52:41 +0100

    finito a parte le statistiche

--- app/controllers/statistiche_controller.rb
+++ app/controllers/statistiche_controller.rb
@@ -1,24 +1,71 @@
 class StatisticheController < ApplicationController
   def costo_per_cat_coltura
-    sql = "Select * from ... your sql query here"
+    sql = "SELECT id, nome from cat_coltures "
     records_array = ActiveRecord::Base.connection.execute(sql)
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: records_array}
+    end
   end
 
   def costo_per_coltura
+    sql = "SELECT id, nome from cat_coltures "
+    records_array = ActiveRecord::Base.connection.execute(sql)
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: records_array}
+    end
   end
 
   def costo_per_operazione
+    sql = "SELECT id, nome from cat_coltures "
+    records_array = ActiveRecord::Base.connection.execute(sql)
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: records_array}
+    end
   end
 
   def costo_carburante_per_cat_coltura
+    sql = "SELECT id, nome from cat_coltures "
+    records_array = ActiveRecord::Base.connection.execute(sql)
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: records_array}
+    end
   end
 
   def costo_carburante_per_coltura
+    sql = "SELECT id, nome from cat_coltures "
+    records_array = ActiveRecord::Base.connection.execute(sql)
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: records_array}
+    end
   end
 
   def costo_carburante_per_operazione
+    sql = "SELECT id, nome from cat_coltures "
+    records_array = ActiveRecord::Base.connection.execute(sql)
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: records_array}
+    end
   end
 
   def costo_carburante_per_attrezzo
+    sql = "SELECT id, nome from cat_coltures "
+    records_array = ActiveRecord::Base.connection.execute(sql)
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: records_array}
+    end
   end
 end
