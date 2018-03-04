798334181c0eaebe284e14ef087631dc42fbc6c7
Filippo <filippo.luppi@gmail.com>
2018-03-04 13:29:05 +0000

    flot chart pie added

--- app/controllers/statistiche_controller.rb
+++ app/controllers/statistiche_controller.rb
@@ -2,8 +2,21 @@ class StatisticheController < ApplicationController
   def costo_per_coltura
     sql = "SELECT  colturas.nome AS Coltura, ROUND(SUM((prezzos.prezzo_fattura-(prezzos.prezzo_fattura*prezzos.sconto/100))*prodotto_trattamentos.dose_ha*colturas.estensione_ha),2) AS Costo, ROUND(SUM(attrezzos.costo_carburante*attrezzos.deperimento*attrezzos.consumo*colturas.estensione_ha), 2) AS Carburante FROM Gestione_development.colturas, Gestione_development.trattamentos,Gestione_development.prodotto_trattamentos,Gestione_development.prodottos,Gestione_development.prezzos,Gestione_development.attrezzos WHERE Gestione_development.colturas.id=Gestione_development.trattamentos.id_coltura AND Gestione_development.trattamentos.id=Gestione_development.prodotto_trattamentos.id_trattamento AND Gestione_development.prodotto_trattamentos.id_prodotto=Gestione_development.prodottos.id AND Gestione_development.prodottos.id=Gestione_development.prezzos.id_prodotto AND Gestione_development.trattamentos.id_attrezzo=Gestione_development.attrezzos.id GROUP BY Coltura,attrezzos.costo_carburante,attrezzos.deperimento,attrezzos.consumo,colturas.estensione_ha "
     records_array = ActiveRecord::Base.connection.execute(sql)
+    require 'net/http'
 
-    respond_to do |format|
+url = URI.parse('http://trattamenti-web-service-filippo95.c9users.io/charts.php?query=carburante')
+req = Net::HTTP::Get.new(url.to_s)
+res = Net::HTTP.start(url.host, url.port) {|http|
+  http.request(req)
+}
+@data=res.body
+url = URI.parse('http://trattamenti-web-service-filippo95.c9users.io/charts.php?query=prodotti')
+req = Net::HTTP::Get.new(url.to_s)
+res = Net::HTTP.start(url.host, url.port) {|http|
+  http.request(req)
+}
+@prodotti=res.body
+     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: records_array}
     end
